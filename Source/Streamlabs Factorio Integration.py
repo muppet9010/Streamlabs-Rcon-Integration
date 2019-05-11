5e464a5907a16b87dc3cdc88d924660c317598f0
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2019-05-11 20:46:17 +0000

    working startup routine and GUI

--- Source/Streamlabs Factorio Integration.py
+++ Source/Streamlabs Factorio Integration.py
@@ -1,36 +1,52 @@
-import socketio
+import socketio as SocketIo
 import tkinter as TK
-import datetime
+import datetime as Datetime
+import requests as Requests
+import json as Json
+import os as Os
 
 
 class Obs():
+    disconnecting = False
+    connecting = False
+
     def __init__(self):
-        self.sio = socketio.Client()
+        self.sio = SocketIo.Client()
         self.sio.on('event', self.EventHandler)
         self.sio.on('connect', self.ConnectHandler)
         self.sio.on('disconnect', self.DisconnectHandler)
 
     def ConnectHandler(self):
-        print('Connected')
-        gui.UpdateStreamlabsStatus()
-        gui.AddToActivityLog("Streamlabs Connected")
+        Logging.DebugLog('Streamlabs Connected')
+        self.disconnecting = False
+        if self.connecting:
+            Gui.StartPostObsConnection()
 
     def DisconnectHandler(self):
-        print('Disconnected')
-        gui.UpdateStreamlabsStatus()
-        gui.AddToActivityLog("Streamlabs Disconnected")
+        Logging.DebugLog('Streamlabs Disconnected')
+        if not self.disconnecting:
+            Gui.AddToActivityLog("Error Streamlabs Stopped Unexpectedly")
+        self.disconnecting = False
+        Gui.UpdateStatus()
 
     def EventHandler(self, msg):
         print('Received message: ', msg)
         ObsEvent(msg)
 
     def Connect(self):
-        if obs.sio.eio.state != "disconnected":
+        if Obs.sio.eio.state != "disconnected":
             return
-        gui.AddToActivityLog("Connecting")
+        Logging.DebugLog("Streamlabs Connecting")
+        self.disconnecting = False
+        self.connecting = True
         self.sio.connect('https://sockets.streamlabs.com?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6IkJFMzU5QjZFNTEzRjczOTk1NDlCIiwicmVhZF9vbmx5Ijp0cnVlLCJwcmV2ZW50X21hc3RlciI6dHJ1ZSwidHdpdGNoX2lkIjoiODk1NjMzNDMifQ.K4F-AFIqFkJPFXrcfSO9_aX8g449BNhRGSngHL40dls')
 
     def Disconnect(self):
+        if Obs.sio.eio.state != "connected":
+            return
+        Logging.DebugLog("Streamlabs Disconnecting")
+        self.disconnecting = True
+        self.connecting = False
         self.sio.disconnect()
 
 
@@ -44,13 +60,14 @@ class ObsEvent():
 
     def GetNormalisedData(self, data):
         if len(data["message"]) != 1:
-            gui.AddToActivityLog("wrong number of payloads in event: " +
+            Gui.AddToActivityLog("wrong number of payloads in event: " +
                                  len(data["message"]) + " data: " + str(data))
             return False
         message = data["message"][0]
         if (self.platform == "streamlabs" and self.type == "donation") or (self.platform == "youtube_account" and self.type == "superchat"):
             self.valueType = "money"
-            self.value = self.GetNormalisedValue(message)
+            self.value = Currency.GetNormalisedValue(
+                message.currency, message.amount)
         elif (self.platform == "twitch_account" and self.type == "bits"):
             self.valueType = "money"
             self.value = message["amount"] / 100
@@ -77,15 +94,10 @@ class ObsEvent():
             self.value = message["viewers"]
 
         if self.value == None or self.valueType == None:
-            gui.AddToActivityLog("Event not recognised: " + str(data))
+            Gui.AddToActivityLog("Event not recognised: " + str(data))
             return False
         else:
             return True
-
-    def GetNormalisedValue(self, message):
-        if message["currency"] == "USD":
-            return message["amount"]
-        return 1  # TODO use a web service to convert from other currencies to USD
 
 
 class GuiWindow():
@@ -108,24 +120,24 @@ class Gui(TK.Frame):
         self.CreateBottomBar(self.master)
 
     def CreateStreamlabs(self, parent):
-        self.streamlabsContainer = TK.Frame(parent)
-        self.streamlabsContainer.pack(fill=TK.X, side=TK.TOP)
+        self.statusContainer = TK.Frame(parent)
+        self.statusContainer.pack(fill=TK.X, side=TK.TOP)
 
-        self.streamlabsStatusText = TK.StringVar()
-        self.UpdateStreamlabsStatus()
-        streamlabsStatusLabel = TK.Label(
-            self.streamlabsContainer, textvariable=self.streamlabsStatusText, height=1, width=30)
-        streamlabsStatusLabel.pack(side=TK.LEFT)
+        self.statusText = TK.StringVar()
+        self.UpdateStatus()
+        statusLabel = TK.Label(
+            self.statusContainer, textvariable=self.statusText, height=1, width=30)
+        statusLabel.pack(side=TK.LEFT)
 
-        streamlabsConnectButton = TK.Button(self.streamlabsContainer)
-        streamlabsConnectButton["text"] = "Connect Streamlabs"
-        streamlabsConnectButton["command"] = obs.Connect
-        streamlabsConnectButton.pack(side=TK.LEFT)
+        startButton = TK.Button(self.statusContainer)
+        startButton["text"] = "Start"
+        startButton["command"] = self.Start
+        startButton.pack(side=TK.LEFT)
 
-        streamlabsDisconnectButton = TK.Button(self.streamlabsContainer)
-        streamlabsDisconnectButton["text"] = "Disonnect Streamlabs"
-        streamlabsDisconnectButton["command"] = obs.Disconnect
-        streamlabsDisconnectButton.pack(side=TK.LEFT)
+        stopButton = TK.Button(self.statusContainer)
+        stopButton["text"] = "Stop"
+        stopButton["command"] = self.Stop
+        stopButton.pack(side=TK.LEFT)
 
     def CreateActivityLog(self, parent):
         titleFrame = TK.LabelFrame(parent, text="Activity Log")
@@ -145,40 +157,129 @@ class Gui(TK.Frame):
         quitButton.pack(side=TK.LEFT)
 
     def Quit(self):
-        obs.Disconnect()
-        logging.Log("Quit")
+        Logging.Log("Quit Button")
+        Obs.Disconnect()
         self.master.destroy()
 
-    def UpdateStreamlabsStatus(self):
-        if obs.sio.eio.state == "connected":
-            self.streamlabsStatusText.set("Streamlabs: Connected")
+    def UpdateStatus(self):
+        if Obs.connecting:
+            self.statusText.set("OBS Connecting")
+        elif Obs.sio.eio.state == "connected":
+            self.statusText.set("Running")
         else:
-            self.streamlabsStatusText.set("Streamlabs: Disconnected")
+            self.statusText.set("Stopped")
 
     def AddToActivityLog(self, text):
-        currentDT = datetime.datetime.now()
-        dtString = currentDT.strftime("%H:%M:%S")
-        activityLine = dtString + " : " + text
-        logging.Log(activityLine)
+        Logging.Log(text)
         self.activityLogText.configure(state="normal")
-        self.activityLogText.insert(1.0, activityLine + "\n")
+        self.activityLogText.insert(1.0, TimestampText(text) + "\n")
         self.activityLogText.configure(state="disabled")
+
+    def Start(self):
+        Logging.DebugLog("Start Button")
+        if not Currency.GetRates():
+            Logging.Log("Error: Get Rates for Currency failed")
+            return
+        Obs.Connect()
+        Gui.UpdateStatus()
+
+    def StartPostObsConnection(self):
+        Obs.connecting = False
+        Gui.UpdateStatus()
+        Gui.AddToActivityLog("Started")
+
+    def Stop(self):
+        Logging.DebugLog("Stop Button")
+        Obs.Disconnect()
+        Gui.AddToActivityLog("Stopped")
+
+
+def TimestampText(text):
+    currentDT = Datetime.datetime.now()
+    dtString = currentDT.strftime("%H:%M:%S")
+    return dtString + " : " + text
+
+
+class Currency():
+    rates = {}
+    cacheFileName = "CurrencyDataCache.json"
+
+    def GetRates(self):
+        if len(self.rates) > 0:
+            return True
+        if Os.path.isfile("./" + self.cacheFileName):
+            Logging.DebugLog("Trying to get currancy rates from cache file")
+            with open(self.cacheFileName, "r") as file:
+                data = Json.load(file)
+            file.closed
+            cacheDateTime = Datetime.date.fromtimestamp(data["timestamp"])
+            currentDateTime = Datetime.date.fromtimestamp(
+                Datetime.datetime.utcnow().timestamp())
+            if currentDateTime <= cacheDateTime:
+                for name, rate in data["quotes"].items():
+                    currency = name[3:]
+                    self.rates[currency] = rate
+                if len(self.rates) > 0:
+                    Logging.DebugLog("Got currency rates from cache file")
+                    return True
+
+        self._SourceRateData()
+        if len(self.rates) > 0:
+            Logging.DebugLog("Got currency rates from website")
+            return True
+        else:
+            Logging.DebugLog(
+                "Failed to get any currency rates from website")
+            return False
+
+    def _SourceRateData(self):
+        Logging.DebugLog("Sourcing currency rates from website")
+        url = "http://www.apilayer.net/api/live"
+        params = {"access_key": "bf92c65503f807f9abd65b83d39c2c6c"}
+        request = Requests.get(url=url, params=params)
+        response = request.json()
+        if not response["success"]:
+            Gui.AddToActivityLog("ERROR: Can't get currency conversion data")
+            return False
+        with open(self.cacheFileName, "w") as file:
+            file.write(Json.dumps(response))
+        file.closed
+        for name, rate in response["quotes"].items():
+            currency = name[3:]
+            self.rates[currency] = rate
+
+    def GetNormalisedValue(self, currency, amount):
+        return amount / self.rates[currency]
 
 
 class Logging():
+    debugLogging = True
+
     def __init__(self):
-        currentDT = datetime.datetime.now()
+        currentDT = Datetime.datetime.now()
         dtString = currentDT.strftime("%Y_%m_%d %H_%M_%S")
         self.logFileName = "Log " + dtString + ".log"
+        self.debugLogFileName = "Debug " + self.logFileName
 
     def Log(self, text):
+        self.DebugLog(text)
         fileName = self.logFileName
-        file = open(fileName, "a")
-        file.write(text + "\n")
-        file.close
+        with open(fileName, "a") as file:
+            file.write(TimestampText(text) + "\n")
+        file.closed
+
+    def DebugLog(self, text):
+        if not self.debugLogging:
+            return
+        fileName = self.debugLogFileName
+        with open(fileName, "a") as file:
+            file.write(TimestampText(text) + "\n")
+        file.closed
 
 
-logging = Logging()
-obs = Obs()
-gui = GuiWindow().gui
-gui.mainloop()
+Logging = Logging()
+Obs = Obs()
+Currency = Currency()
+Gui = GuiWindow().gui
+Logging.DebugLog("App Started")
+Gui.mainloop()
